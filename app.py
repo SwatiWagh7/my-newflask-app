cc32fa7668b01f3033787b074f6c03419c0d7852
SwatiWagh7 <swatiwagh717@gmail.com>
2026-03-31 10:18:26 +0530

    test auto trigger

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DevOps CI/CD Auto Deployment</title>
+    <title>My New DevOps CI/CD Auto Deployment</title>
     <style>
         body {
             font-family: Arial, sans-serif;
